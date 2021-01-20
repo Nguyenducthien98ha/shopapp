6560b857d0b25d0c31febc4ac55ac6394b5566d5
Rich Hildred <updatee>
2021-01-20 10:27:41 +0700

    Enter

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shopapp/screens/home/components/categoties.dart';
-import 'package:shopapp/screens/home/components/section_title.dart';
+import 'package:shopapp/screens/home/components/popular_products.dart';
+import 'package:shopapp/screens/home/components/special_offers.dart';
 import 'package:shopapp/size_config.dart';
 import 'discount_banner.dart';
 import 'home_header.dart';
@@ -14,41 +15,17 @@ class Body extends StatelessWidget {
       child: SingleChildScrollView(
         child: Column(
           children: [
-            SizedBox(height: getProportionateScreenWidth(20),),
+            SizedBox(height: getProportionateScreenWidth(10),),
             HomeHeader(),
-            SizedBox(height: getProportionateScreenWidth(30),),
+            SizedBox(height: getProportionateScreenWidth(10),),
             DiscountBanner(),
-            SizedBox(height: getProportionateScreenWidth(30),),
+            SizedBox(height: getProportionateScreenWidth(10),),
             Categories(),
-            SizedBox(height: getProportionateScreenWidth(30),),
-            SectionTitle(
-              text: "Special for you",
-              press: (){},
-            ),
-            SizedBox(
-              width: getProportionateScreenWidth(242),
-              height: getProportionateScreenWidth(100),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(20),
-                child: Stack(
-                  children: [
-                    Image.asset(
-                      "assets/images/Image Banner 2.png",fit: BoxFit.cover,),
-                      Container(
-                        decoration: BoxDecoration(
-                          gradient: LinearGradient(
-                            begin: Alignment.topCenter,
-                            end: Alignment.bottomCenter,
-                            colors: [
-                              Color(0xFF343434).withOpacity(0.4),
-                              Color(0xFF343434).withOpacity(0.15),
-                            ]
-                          )
-                        ),
-                      )
-                  ],
-                )),
-            )
+            SizedBox(height: getProportionateScreenWidth(10),),
+            SpecialOffers(),
+            SizedBox(height: getProportionateScreenWidth(10),),
+            PopularProducts(),
+            SizedBox(height: getProportionateScreenWidth(10),),
           ],
         ),
       ),
